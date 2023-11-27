e6b4618312304f9b8b1e90669f7b72b4882fa220
MerayP <dpinedam@unah.hn>
2023-11-27 15:04:45 -0600

    areglo

--- tests/Feature/CambiarContraTest.php
+++ tests/Feature/CambiarContraTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -12,37 +11,44 @@ use Tests\TestCase;
 class CambiarContraTest extends TestCase
 {
 
-    use RefreshDatabase;
+    //Esta es una prueba que verifica si la página que contiene el formulario de cambiar contraseña está disponible y devuelve un 
+    //código de estado HTTP 200.
+    public function testCambiarContraFormIsAvailable()
+    {
+        $response = $this->get('/egresado/contrasenia');
+        $response->assertStatus(200);
+    }
 
     public function testChangePassword()
     {
         // Crea un usuario de prueba
-        //se modifico la estructura de la factory para poder trabajar con laravel 8
-        $user = User::factory()->create([
-            'id' => 2,
+        $user = factory(User::class)->create([
+            'name' => "Juan Pablo Perez",
+            'correo' => "pablojuan@gmail.com",
+            'nacimiento' => "08-10-01",
+            'username' => "pablo",
+            'password' => "pablo123",
+            'identidad' => "0703200103082",
+            'telefono' => "9856-2300",
+            'estado' => 1,
         ]);
-        
+
         // Iniciar sesión como el usuario
         Auth::login($user);
 
         // Datos de prueba para cambiar la contraseña
         $newPassword = 'nueva_contra123';
 
-        //la URL estaba mala
-        $response = $this->post('contrasenia', [
-            'viejapassword' => 'password', // Contraseña actual
-            'password' => $newPassword, // Nueva contraseña
-            'password_confirmation' => $newPassword // Confirmación de la nueva contraseña
+        $response = $this->post('contrasenia.cambiar', [
+            'password' => $newPassword,
+            'password_confirmation' => $newPassword,
         ]);
 
-        //refrescamos los datos para efectuar los cambios
-        $user->refresh();
         // Verifica que la contraseña se haya actualizado en la base de datos
         $this->assertTrue(Hash::check($newPassword, $user->password));
 
         // Verifica que se haya redirigido a la página anterior con el mensaje de éxito
-        //el mensaje de error y la variable era incorrecto
-        $response->assertRedirect()->assertSessionHas('mensaje', 'La contraseña fue actualizada exitosamente.');
+        $response->assertRedirect()->assertSessionHas('passwordStatus', 'Password successfully updated.');
 
         // Cierra la sesión del usuario
         Auth::logout();
@@ -51,7 +57,7 @@ class CambiarContraTest extends TestCase
     public function testChangePasswordNotAllowedForDefaultUser()
     {
         // Crea un usuario de prueba con ID 1 (default user)
-        $user = User::factory()->create([
+        $user = factory(User::class)->create([
             'id' => 1,
         ]);
 
@@ -61,8 +67,7 @@ class CambiarContraTest extends TestCase
         // Datos de prueba para cambiar la contraseña
         $newPassword = 'nueva_contra123';
 
-        //la URL estaba mala
-        $response = $this->post('contrasenia', [
+        $response = $this->post('contrasenia.cambiar', [
             'password' => $newPassword,
             'password_confirmation' => $newPassword,
         ]);
@@ -71,8 +76,7 @@ class CambiarContraTest extends TestCase
         $this->assertFalse(Hash::check($newPassword, $user->password));
 
         // Verifica que se haya redirigido de vuelta con un mensaje de error
-        //el mensaje de error y la variable era incorrecto
-        $response->assertRedirect()->assertSessionHasErrors();
+        $response->assertRedirect()->assertSessionHasErrors('not_allow_password');
 
         // Cierra la sesión del usuario
         Auth::logout();
